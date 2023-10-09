3d6b36ecbe92ec5b8d10877b16e333ab7ff3f0c0
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-10-08 20:33:12 -0700

    Add About Icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,28 @@
 import { Card } from "@mui/material";
 
 function About() {
-  const imageUrl = new URL("../assets/anthony_hsu.jpg", import.meta.url).href
+  const imageUrl = new URL("../assets/anthony_hsu.jpg", import.meta.url).href;
   return (
     <div className="about-container">
       <Card className="about-image-card" elevation={20} square>
-        <img src={imageUrl} className="about-image"/>
+        <img src={imageUrl} className="about-image" />
       </Card>
       <Card className="about-text-card" elevation={10} square>
-        <div className="about-text">
           <h1 className="about-text-title">About Me</h1>
-          <p className="about-text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+          <p className="about-text-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="about-icon-container">
+            <i className="devicon-github-original colored about-icon"></i>
+            <i className="devicon-linkedin-plain about-icon"></i>
+            <i className="devicon-twitter-original about-icon"></i>
+          </div>
       </Card>
     </div>
   );
