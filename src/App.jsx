34d5b40f79fab9909bbff5fdc7b9169037173805
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-10-10 21:48:44 -0700

    Add Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
 
 function App() {
   return <div className="App">
     <About />
     <Portfolio />
+    <Contact />
   </div>;
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,49 @@
+function Contact() {
+  const imageUrl = new URL("../assets/contact-photo.jpg", import.meta.url).href;
+  const githubLink = "https://github.com/anthony-hsu/";
+  const linkedinLink = "https://www.linkedin.com/in/anthony-hsu-73a28985/";
+
+  const openLink = (link) => {
+    window.open(link);
+  };
+  return (
+    <div className="contact-container">
+      <div className="contact-text">
+        <h1 className="contact-title">Contact Me</h1>
+        <p className="contact-message contact-body">
+          Please get in touch if you think our work could be mutually
+          beneficial!
+        </p>
+        <div className="contact-address">
+          <p className="contact-address-street contact-body">123 Fake Street</p>
+          <p className="contact-address-city contact-body">San Diego, California 92103</p>
+        </div>
+        <div className="contact-phone">
+          {/* icon */}
+          <p className="contact-phone-number contact-body">123-456-7890</p>
+        </div>
+        <div className="contact-email">
+          {/* icon */}
+          <p className="contact-email-address contact-body">anthhsu@gmail.com</p>
+        </div>
+        <div className="contact-icons">
+          <i
+            className="devicon-github-original colored about-icon icon"
+            onClick={() => openLink(githubLink)}
+          ></i>
+          <i
+            className="devicon-linkedin-plain about-icon icon"
+            onClick={() => openLink(linkedinLink)}
+          ></i>
+          <i className="devicon-twitter-original about-icon icon"></i>
+        </div>
+      </div>
+      <div className="contact-image-container">
+
+      <img src={imageUrl} className="contact-image" />
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
