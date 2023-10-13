399ebdffdb57e15662573f1590356315a9ef2c4d
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-10-12 22:41:53 -0700

    Add media queries for mobile view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,12 +10,37 @@ function About() {
   };
 
   return (
-    <div className="about-container">
-      <Card className="about-image-card" elevation={20} square>
-        <h1 className="about-name">Anthony Hsu</h1>
-        <img src={imageUrl} className="about-image" />
-      </Card>
-      <Card className="about-text-card" elevation={10} square>
+    <>
+      <div className="about-container">
+        <Card className="about-image-card" elevation={20} square>
+          <h1 className="about-name">Anthony Hsu</h1>
+          <img src={imageUrl} className="about-image" />
+        </Card>
+        <Card className="about-text-card-desktop" elevation={10} square>
+          <h1 className="about-text-title">About Me</h1>
+          <p className="about-text-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="about-icon-container">
+            <i
+              className="devicon-github-original colored about-icon icon"
+              onClick={() => openLink(githubLink)}
+            ></i>
+            <i
+              className="devicon-linkedin-plain about-icon icon"
+              onClick={() => openLink(linkedinLink)}
+            ></i>
+            <i className="devicon-twitter-original about-icon icon"></i>
+          </div>
+        </Card>
+      </div>
+      <div className="about-text-card-mobile">
         <h1 className="about-text-title">About Me</h1>
         <p className="about-text-body">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -37,8 +62,8 @@ function About() {
           ></i>
           <i className="devicon-twitter-original about-icon icon"></i>
         </div>
-      </Card>
-    </div>
+      </div>
+    </>
   );
 }
 
