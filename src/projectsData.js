b77fd5d723dab87a61406169115deb43c2ec06be
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-12-08 17:35:49 -0800

    Add screenshot for online-store

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,6 +1,7 @@
 const cvImageUrl = new URL("./assets/cv-builder.png", import.meta.url).href;
-const memoryGameUrl = new URL("./assets/memory-game.png", import.meta.url).href;
-const wordGameUrl = new URL("./assets/word-game.png", import.meta.url).href;
+const memoryGameImageUrl = new URL("./assets/memory-game.png", import.meta.url).href;
+const wordGameImageUrl = new URL("./assets/word-game.png", import.meta.url).href;
+const onlineStoreImageUrl = new URL("./assets/online-store.png", import.meta.url).href;
 
 const projects = [
   {
@@ -13,18 +14,24 @@ const projects = [
     liveLink: "https://personal-budget-app-anthony-hsu.netlify.app",
   },
   {
+    title: "Wordle Clone",
+    description: "Try your best to guess the word! Now with the ability to configure word length and number of attempts.",
+    image: wordGameImageUrl,
+    githubLink: "https://github.com/anthony-hsu/word-game",
+    liveLink: "https://word-game-anthony-hsu.netlify.app",
+  },
+  {
     title: "Online Store",
     description:
       "Online Store gives you the freedom to buy whatever you want, whenever you want.",
-    image:
-      "https://screenshot-proxy.netlify.app/f_avif,fit_cover,s_336x210/https://d33wubrfki0l68.cloudfront.net/651a3d9f2cde840008866d70/screenshot_2023-10-02-03-49-05-0000.png",
+    image: onlineStoreImageUrl,
     githubLink: "https://github.com/anthony-hsu/online-store",
     liveLink: "https://online-store-anthony-hsu.netlify.app",
   },
   {
     title: "Memory Card Game",
     description: "Test your memory with this fun game. Can you choose them all?",
-    image: memoryGameUrl,
+    image: memoryGameImageUrl,
     githubLink: "https://github.com/anthony-hsu/memory-card-game",
     liveLink: "https://memory-card-game-anthony-hsu.netlify.app",
   },
@@ -34,13 +41,6 @@ const projects = [
     image: cvImageUrl,
     githubLink: "https://github.com/anthony-hsu/cv-project",
     liveLink: "https://cv-builder-anthony-hsu.netlify.app",
-  },
-  {
-    title: "Wordle Clone",
-    description: "Try your best to guess the word! Now with the ability to configure word length and number of attempts.",
-    image: wordGameUrl,
-    githubLink: "https://github.com/anthony-hsu/word-game",
-    liveLink: "https://word-game-anthony-hsu.netlify.app",
   },
 ];
 
