ce5cb21566a111f833704779bdb72e4b5e0e457a
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-12-10 23:00:56 -0800

    Add screenshot and site name for BDGT

--- src/projectsData.js
+++ src/projectsData.js
@@ -2,16 +2,16 @@ const cvImageUrl = new URL("./assets/cv-builder.png", import.meta.url).href;
 const memoryGameImageUrl = new URL("./assets/memory-game.png", import.meta.url).href;
 const wordGameImageUrl = new URL("./assets/word-game.png", import.meta.url).href;
 const onlineStoreImageUrl = new URL("./assets/online-store.png", import.meta.url).href;
+const bdgtImageUrl = new URL("./assets/bdgt.png", import.meta.url).href;
 
 const projects = [
   {
-    title: "Personal Budget App",
+    title: "Budgeting App",
     description:
       "Budget your finances so that you have the freedom to buy whatever you want.",
-    image:
-      "https://screenshot-proxy.netlify.app/f_avif,fit_cover,s_336x210/https://d33wubrfki0l68.cloudfront.net/651cb6f2502b06000897bdc5/screenshot_2023-10-04-00-51-28-0000.png",
+    image: bdgtImageUrl,
     githubLink: "https://github.com/anthony-hsu/personal-budget-app",
-    liveLink: "https://personal-budget-app-anthony-hsu.netlify.app",
+    liveLink: "https://bdgt-anthony-hsu.netlify.app",
   },
   {
     title: "Wordle Clone",
